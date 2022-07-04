a0a3a2b7737a5d9342d860c3405af0c5321a8ef1
mesutbarut12 <mesutbarut863@gmail.com>
2022-07-04 11:35:07 +0200

    MVVM packages created

--- app/src/main/java/com/barut/zahlerstand/view/MainActivity.kt
+++ app/src/main/java/com/barut/zahlerstand/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.barut.zahlerstand
+package com.barut.zahlerstand.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.barut.zahlerstand.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/barut/zahlerstand/view/MainFragment.kt
+++ app/src/main/java/com/barut/zahlerstand/view/MainFragment.kt
@@ -1,10 +1,11 @@
-package com.barut.zahlerstand
+package com.barut.zahlerstand.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.barut.zahlerstand.R
 
 
 class MainFragment : Fragment() {
